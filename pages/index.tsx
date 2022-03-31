a6f3509f375aab302645eae90bb8bf0ed2246310
RyosukeCla <RyosukeCla@users.noreply.github.com>
2022-03-31 10:41:31 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home = (props: Props & { hasMore: boolean }) => {
         filter
       />
       <h1 data-title>Hello!</h1>
-      <p>I am Ryosuke Suzuki, a software engineer in Tokyo, preferring to work at a startup, enjoying singing, born in 1996.</p>
+      <p>I am Ryosuke Suzuki, a software engineer in Tokyo, preferring to work at a startup, enjoying singing, born in 1996, finished a degree in mathmatics in meiji university.</p>
 
       <h4>Recently</h4>
       <Docs docs={props.docs}/>
